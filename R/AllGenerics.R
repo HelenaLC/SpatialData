f626ba6850ef4889afefda2af143686d20691a9d
const-ae <artjom31415@googlemail.com>
2023-04-27 16:39:16 +0200

    Add element accessor functions and remove data accessor

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,3 +1,4 @@
+
 #' @export
 setGeneric("images", function(x, ...) standardGeneric("images"))
 
@@ -9,6 +10,18 @@ setGeneric("shapes", function(x, ...) standardGeneric("shapes"))
 
 #' @export
 setGeneric("points", function(x, ...) standardGeneric("points"))
+
+#' @export
+setGeneric("image", function(x, ...) standardGeneric("image"))
+
+#' @export
+setGeneric("label", function(x, ...) standardGeneric("label"))
+
+#' @export
+setGeneric("shape", function(x, ...) standardGeneric("shape"))
+
+#' @export
+setGeneric("point", function(x, ...) standardGeneric("point"))
 
 setGeneric("imageNames", function(x, ...) standardGeneric("imageNames"))
 setGeneric("labelNames", function(x, ...) standardGeneric("labelNames"))
