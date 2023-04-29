6c6b64ff5d0d1800d59a36e2b3a00f32747e8160
HelenaLC <helena@crowells.eu>
2023-04-29 13:29:39 +0200

    code cleaning

--- R/writeImageArray.R
+++ R/writeImageArray.R
@@ -1,22 +1,24 @@
 #' @rdname writeImageArray
-#' @title Read as `ImageArray`
+#' @title Write `ImageArray` to Zarr-array
 #' @description ...
-#' 
-#' @param image A "ImageArray" specifying the image 
+#'
+#' @param image A "ImageArray" specifying the image
 #'   to be saved.
 #' @param path A character string specifying
 #'   a path
 #' @param ... Further arguments to be passed to write_zarr_array.
-#' 
+#'
+#' @return \code{NULL}
+#'
 #' @examples
 #' path <- "/path/to/my/image.zarr"
 #' path <- system.file(path, package = "SpatialData")
 #' writeImageArray(image, path)
-#' 
+#'
 #' @importFrom jsonlite toJSON
 #' @importFrom Rarr write_zarr_array
 #' @export
-writeImageArray <- function(image, path, ...) { 
+writeImageArray <- function(image, path, ...) {
   stopifnot("image must be of type 'ImageArray'" =  is(image, "ImageArray"))
   stopifnot("path must be of type 'character'" =  is.character(path))
 
@@ -42,7 +44,7 @@ writeImageArray <- function(image, path, ...) {
 
   # get metadata and write to file
   metadata <- toJSON(image@metadata)
-  
+
   # TODO: once this is fixed https://github.com/grimbough/Rarr/issues/1 adapt
   write(metadata, file.path(paste0(path,path),"/.zattrs"))
 }
