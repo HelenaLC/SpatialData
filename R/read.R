099b1d9c31a7d3946660359269f44c644e2caffe
Louise Deconinck <louise.deconinck@gmail.com>
2024-11-12 16:17:34 +0100

    Add option to use anndataR to read in tables

--- R/read.R
+++ R/read.R
@@ -12,6 +12,9 @@
 #'   Control which elements should be read for each layer.
 #'   The default, NULL, reads all elements; alternatively, may be FALSE 
 #'   to skip a layer, or a integer vector specifying which elements to read.
+#' @param anndataR, logical, default FALSE
+#'   Uses \code{anndataR} to read tables if TRUE.
+#'   Uses \code{basilisk}, \code{anndata} and \code{zellkonverter} to read tables if FALSE.
 #'
 #' @return 
 #' \itemize{
@@ -31,10 +34,6 @@ NULL
 #' @importFrom jsonlite fromJSON
 #' @export
 readImage <- function(x, ...) {
-  if(x == ""){
-    za_list = list()
-    meta = Zattrs()
-  } else {
     md <- fromJSON(file.path(x, ".zattrs"))
     paths <- .get_multiscales_dataset_paths(md)
     za_list <- sapply(paths, function(ps){
@@ -49,7 +48,7 @@ readImage <- function(x, ...) {
 #' @importFrom Rarr ZarrArray
 #' @importFrom jsonlite fromJSON
 #' @export
-readLabel <- function(x) {
+readLabel <- function(x, ...) {
     md <- fromJSON(file.path(x, ".zattrs"))
     za <- ZarrArray(file.path(x, "0"))
     LabelArray(data=za, meta=Zattrs(md))
@@ -59,7 +58,7 @@ readLabel <- function(x) {
 #' @importFrom jsonlite fromJSON
 #' @importFrom arrow open_dataset
 #' @export
-readPoint <- function(x) {
+readPoint <- function(x, ...) {
     md <- fromJSON(file.path(x, ".zattrs"))
     pq <- list.files(x, "\\.parquet$", full.names=TRUE)
     PointFrame(data=open_dataset(pq), meta=Zattrs(md))
@@ -69,7 +68,7 @@ readPoint <- function(x) {
 #' @importFrom jsonlite fromJSON
 #' @importFrom arrow open_dataset
 #' @export
-readShape <- function(x) {
+readShape <- function(x, ...) {
     require(geoarrow, quietly=TRUE)
     md <- fromJSON(file.path(x, ".zattrs"))
     # TODO: previously had read_parquet(), 
@@ -83,13 +82,21 @@ readShape <- function(x) {
     pkgname="SpatialData", envname="anndata_env",
     packages=c("anndata==0.9.1", "zarr==2.14.2", "spatialdata==0.2.3", "spatialdata-io==0.1.4"))
 
+readTable <- function(x, anndataR = FALSE) {
+    if(anndataR) {
+        readTable_anndataR(x)
+    } else {
+        readTable_basilisk(x)
+    }
+}
+
 #' @rdname readSpatialData
 #' @importFrom reticulate import
 #' @importFrom zellkonverter AnnData2SCE
 #' @importFrom SingleCellExperiment int_metadata<-
 #' @importFrom basilisk basiliskStart basiliskStop basiliskRun
 #' @export
-readTable <- function(x) {
+readTable_basilisk <- function(x) {
     # TODO: temporary, until AnnDataR supports
     # reading AnnData from .zarr (.h5ad only atm)
     proc <- basiliskStart(.env)
@@ -106,14 +113,31 @@ readTable <- function(x) {
 }
 
 #' @rdname readSpatialData
+#' @importFrom anndataR read_zarr to_SingleCellExperiment
 #' @export
-readSpatialData <- function(x, images=TRUE, labels=TRUE, points=TRUE, shapes=TRUE, tables=TRUE) {
+readTable_anndataR <- function(x) {
+    adata <- anndataR::read_zarr(x)
+    anndataR::to_SingleCellExperiment(adata)
+}
+
+#' @rdname readSpatialData
+#' @importFrom anndataR read_zarr to_SingleCellExperiment
+#' @export
+readTable_anndataR <- function(x) {
+    adata <- anndataR::read_zarr(x)
+    anndataR::to_SingleCellExperiment(adata)
+}
+
+#' @rdname readSpatialData
+#' @export
+readSpatialData <- function(x, images=NULL, labels=NULL, points=NULL, shapes=NULL, tables=NULL, anndataR=FALSE) {
+    # TODO: validity checks
     args <- as.list(environment())[.LAYERS]
     skip <- vapply(args, isFALSE, logical(1))
     lapply(.LAYERS[!skip], \(i) {
         j <- list.files(file.path(x, i), full.names=TRUE)
         if (is.numeric(args[[i]])) j <- j[args[[i]]]
         i <- paste0(toupper(substr(i, 1, 1)), substr(i, 2, nchar(i)-1))
-        setNames(lapply(j, get(paste0("read", i))), basename(j))
+        setNames(lapply(j, get(paste0("read", i)), anndataR = anndataR), basename(j))
     }) |> do.call(what=SpatialData)
 }
