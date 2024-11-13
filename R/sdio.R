3467594d7e8a3adda13a4b1cdf9d34bd7ecce9fd
vjcitn <stvjc@channing.harvard.edu>
2024-11-13 12:22:14 +0100

    use_sdio is rational now

--- R/sdio.R
+++ R/sdio.R
@@ -1,14 +1,10 @@
-bsklenv_sdio <- basilisk::BasiliskEnvironment(envname="bsklenv",
-    pkgname="SpatialData",
-    packages=c("spatialdata==0.2.3", "spatialdata-io==0.1.4"))
-
 
 #' enumerate modules
 #' @examples
 #' available_sdio()
 #' @export
 available_sdio = function() {
- proc = basilisk::basiliskStart(bsklenv_sdio, testload="spatialdata") # avoid package-specific import
+ proc = basilisk::basiliskStart(.env, testload="spatialdata") # avoid package-specific import
  on.exit(basilisk::basiliskStop(proc))
  basilisk::basiliskRun(proc, function() {
      sdio = reticulate::import("spatialdata_io")
@@ -16,18 +12,29 @@ available_sdio = function() {
    })
 }
 
-#' use a reader
+#' use spatialdata-io to transform manufacturer output to zarr with specific folder structure
+#' @param platform character(1) must be an element of `available_sdio()` output
+#' @param srcdir character(1) path to folder holding manufacturer output files
+#' @param dest character(1) a path to a desired destination for zarr representation
 #' @examples
-#' use_sdio("xenium") # not basilisk-standards compliant -- leaks python reference into R
+#' p1 = path_to_10x_xen_demo()
+#' td = tempfile()
+#' dir.create(td)
+#' unzip(p1, exdir=td)
+#' target = tempfile()
+#' use_sdio("xenium", srcdir=td, dest=target)
+#' br2fov = SpatialData::readSpatialData(target)
+#' br2fov
 #' @export
-use_sdio = function(platform="xenium") {
- proc = basilisk::basiliskStart(bsklenv_sdio, testload="spatialdata") # avoid package-specific import
+use_sdio = function(platform="xenium", srcdir, dest) {
+ if (dir.exists(dest)) stop("won't write to existing folder, give non-existent path")
+ proc = basilisk::basiliskStart(.env, testload="spatialdata") # avoid package-specific import
  on.exit(basilisk::basiliskStop(proc))
- basilisk::basiliskRun(proc, function(platform) {
+ basilisk::basiliskRun(proc, function(platform, srcdir, dest) {
      sdio = reticulate::import("spatialdata_io")
      avail = names(sdio)
      stopifnot(platform %in% available_sdio())
-     sdio[[platform]]
-   }, platform=platform)
+     sdio[[platform]](srcdir)$write(dest)
+   }, platform=platform, srcdir=srcdir, dest=dest)
 }
 
