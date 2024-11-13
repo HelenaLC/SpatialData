56301c09f77a8f7026faf6e28c9745a7c3c46232
HelenaLC <helena@crowells.eu>
2024-11-13 09:39:48 +0100

    def $ methods

--- R/Zattrs.R
+++ R/Zattrs.R
@@ -14,3 +14,11 @@ Zattrs <- \(x=list()) {
 # TODO: ideally some valid empty constructor for each type of element,
 # e.g., .zattrs are different for point/label/shape/image elements;
 # simplest would be xyz (time, channel), identity transformation etc. 
+
+#' @importFrom utils .DollarNames
+#' @export
+.DollarNames.Zattrs <- \(x, pattern="") names(x)
+
+#' @rdname SpatialData
+#' @exportMethod $
+setMethod("$", "Zattrs", \(x, name) x[[name]])
