0366c1a7d3b50374d384cc47801d43cf3d065c98
Dario <dario.righelli@gmail.com>
2024-11-13 15:56:59 +0100

    dividing validities by data structures

--- R/validity.R
+++ R/validity.R
@@ -1,15 +1,51 @@
+# TODO: everything...
+
+.tables_validity <- function(object) {
+    nt <- length(tables(object))
+    for (i in c(1:nt)) {
+        sce <- table(object, i)
+        if (!is("SingleCellExperiment", sce)) {
+            msg <- c(msg, paste0("Table ", i, " is not a SingleCellExperiment"))
+        }
+        md <- SingleCellExperiment::metadata(se)[[1]]
+        if (!all(c("region_key", "instance_key") %in% names(md))) {
+            msg <- c(msg, paste0("region_key/instance_key not present in ",
+                                 i, "-th sce metadata"))
+        }
+    }
+    return(msg)
+}
+
+.points_validity <- function(object) {
+    # Checks if the points have the x,y coordinates, as they are hard-coded
+    # in the plot functions
+    if (!is.null(data(point(object))))
+    {
+        np <- length(points(object))
+        for (i in c(1:np)) {
+            dfi <- data(point(object, i))
+            if (!all(c("x", "y") %in% names(dfi))) {
+                msg <- c(msg, paste0("'x' and 'y' missing in data point ", i))
+            }
+        }
+    }
+    return(msg)
+}
+
 #' @importFrom methods is
 .validateSpatialData <- \(x) {
     typ <- c(
-        images="ImageArray", 
-        labels="LabelArray", 
-        points="PointFrame", 
-        shapes="ShapeFrame", 
+        images="ImageArray",
+        labels="LabelArray",
+        points="PointFrame",
+        shapes="ShapeFrame",
         tables="SingleCellExperiment")
     msg <- NULL
-    for (. in names(typ)) if (length(x[[.]])) 
+    for (. in names(typ)) if (length(x[[.]]))
         if (!all(vapply(x[[.]], \(y) is(y, typ[.]), logical(1))))
             msg <- c(msg, sprintf("'%s' should be a list of '%s'", ., typ[.]))
+    msg <- c(msg, .points_validity(object))
+    msg <- c(msg, .tables_validity(object))
     if (length(msg))
         return(msg)
     return(TRUE)
