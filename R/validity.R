f66df422a8e1ec02251d4ed599b02750312b1d90
HelenaLC <helena@crowells.eu>
2024-11-13 16:40:53 +0100

    more validity checks on LabelArray .zattrs

--- R/validity.R
+++ R/validity.R
@@ -19,20 +19,41 @@
 #' @importFrom S4Vectors setValidity2
 setValidity2("SpatialData", .validateSpatialData)
 
+.validateZattrs_multiscales <- \(x, mgs) {
+    if (is.null(ms <- x$multiscales))
+        msg <- c(msg, "missing 'multiscales'")
+    # MUST contain
+    for (. in c("axes", "datasets")) 
+        if (is.null(ms[[.]]))
+            msg <- c(msg, sprintf("missing 'multiscales$%s'", .))
+    return(msg)
+}
+.validateZattrs_axes <- \(x, msg) {
+    if (!is.list(ax <- x$axes))
+        msg <- c(msg, "missing or non-list 'axes'")
+    ax <- ax[[1]]
+    if (is.null(ax$name)) 
+        msg <- c(msg, "missing 'axes$name'")
+    if (!is.null(ts <- ax$type)) 
+        if (!all(ts %in% c("space", "time", "channel")))
+            msg <- c(msg, "'axes$type' should be 'space/time/channel'")
+    return(msg)
+}
+.validateZattrs_coordTrans <- \(x, msg) {
+    if (!is.list(ct <- x$coordinateTransformations))
+        msg <- c(msg, "missing or non-list 'coordTrans'")
+    ct <- ct[[1]]
+    for (. in c("input", "output", "type")) 
+        if (is.null(ct[[.]]))
+            msg <- c(msg, sprintf("'coordTrans' missing '%s'", .))
+    return(msg)
+}
 .validateZattrsLabelArray <- \(x) {
     msg <- c()
     za <- meta(x)
-    if (is.null(ms <- za$multiscales))
-        msg <- c(msg, "missing 'multiscales'")
-    if (!is.list(ax <- ms$axes))
-        msg <- c(msg, "missing or non-list 'axes'")
-    ax <- ax[[1]]
-    if (!all(dim(ax) == 2))
-        msg <- c(msg, "'axes' should have dim. 2x2")
-    if (!is.list(ct <- ms$coordinateTransformations))
-        msg <- c(msg, "missing or non-list 'coordinateTransformations'")
-    ct <- ct[[1]]
-    for (. in c("input", "output", "type")) if (is.null(ct[[.]]))
-        msg <- c(msg, sprintf("'coordinateTransformations' missing '%s'", .))
+    msg <- .validateZattrs_multiscales(za, msg)
+    ms <- za$multiscales
+    msg <- c(msg, .validateZattrs_axes(ms, msg))
+    msg <- c(msg, .validateZattrs_coordTrans(ms, msg))
     if (length(msg)) return(msg) else return(TRUE)
 }
