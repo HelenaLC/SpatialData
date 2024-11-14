3f0821520a7f78ddee188c265d65ef9d2b24f8e2
Dario <dario.righelli@gmail.com>
2024-11-14 12:10:12 +0100

    adding description to the LabelArray

--- R/LabelArray.R
+++ R/LabelArray.R
@@ -4,9 +4,17 @@
 #' @param x \code{LabelArray}
 #' @param data list of \code{\link[Rarr]{ZarrArray}}s
 #' @param meta \code{\link{Zattrs}}
-#' @param metadata optional list of arbitrary 
+#' @param metadata optional list of arbitrary
 #'   content describing the overall object.
 #' @param ... option arguments passed to and from other methods.
+#' @description
+#' The `LabelArray` class (LA) is defined to store labels in `array` or
+#' `data.frame` format into the dedicated `@data` slot together with the `meta`
+#' attributes defined in the Zattr files.
+#' Optional metadata can be stored in the `metadata` list.
+#'
+#' Defined methods are:
+#' - dim: returns the dimensions of the `@data` slot.
 #'
 #' @return \code{LabelArray}
 #'
