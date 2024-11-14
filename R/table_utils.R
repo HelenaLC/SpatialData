71c50a8d831d8e8c78ca3993843dc1103447aaf2
Louise Deconinck <louise.deconinck@gmail.com>
2024-11-14 09:21:35 +0100

    expand vignette

--- R/table_utils.R
+++ R/table_utils.R
@@ -20,7 +20,7 @@ setMethod("getTable", c("SpatialData", "ANY", "ANY"), \(x, region = NULL, table_
     }
     
     if (!is.null(table_name)) {
-        return(tables(x)$table_name)
+        return(tables(x)[[table_name]])
     }
 
     if (!is.null(region)) {
@@ -45,3 +45,24 @@ setMethod("getElementAnnotators", c("SpatialData", "character"), \(x, element) {
     })
     annotators[lengths(annotators) != 0] # filter out when no table was found
 })
+
+# very very basic
+# tested with PointArray
+getValues <- function(value_key, element=NULL, sdata=NULL, element_name=NULL, table_name=NULL) {
+    if(!is.null(element) && value_key %in% names(element)){
+        return(element[[value_key]])
+    }
+
+    if(!is.null(sdata) && !is.null(element_name) && !is.null(table_name)){
+        table <- getRegionData(sdata, re = element_name)
+        if(value_key %in% rownames(table)){
+            return(table[value_key])
+        }
+        if(value_key %in% colnames(table)){
+            return(table[value_key])
+        }
+    }
+
+}
+
+#get_values(value_key="channel_0_sum", sdata=sdata, element_name="blobs_labels", table_name="table")
