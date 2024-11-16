2e1f0d42dbbd3b6d6c059c338134dac837e97c4f
HelenaLC <helena@crowells.eu>
2024-11-16 18:37:26 +0100

    +axes() methods for Zattrs including tests

--- R/Zattrs.R
+++ R/Zattrs.R
@@ -21,6 +21,20 @@ Zattrs <- \(x=list()) {
 #' @export
 .DollarNames.Zattrs <- \(x, pattern="") names(x)
 
-#' @rdname SpatialData
+#' @rdname Zattrs
 #' @exportMethod $
 setMethod("$", "Zattrs", \(x, name) x[[name]])
+
+setGeneric("axes", \(x, ...) standardGeneric("axes"))
+
+#' @rdname Zattrs
+#' @export
+setMethod("axes", "Zattrs", \(x, ...) {
+    if (!is.null(ms <- x$multiscales)) x <- ms
+    if (is.null(x <- x$axes)) stop("couln't find 'axes'") 
+    if (is.character(x)) x else x[[1]]
+})
+
+#' @rdname Zattrs
+#' @export
+setMethod("axes", "SpatialDataElement", \(x, ...) axes(meta(x)))
