795dfe66ed9d98245453a51a8028ac1636d1c463
HelenaLC <helena@crowells.eu>
2024-11-16 18:20:38 +0100

    add methods for two-index subsetting

--- R/ShapeFrame.R
+++ R/ShapeFrame.R
@@ -47,8 +47,7 @@ setMethod("length", "ShapeFrame", \(x) nrow(data(x)))
 
 #' @rdname ShapeFrame
 #' @export
-setMethod("names", "ShapeFrame", \(x) {
-    setdiff(names(data(x)), "__null_dask_index__") })
+setMethod("names", "ShapeFrame", \(x) names(data(x)))
 
 #' @importFrom utils .DollarNames
 #' @export
@@ -60,10 +59,28 @@ setMethod("names", "ShapeFrame", \(x) {
 #' @exportMethod $
 setMethod("$", "ShapeFrame", \(x, name) data(x)[[name]])
 
+# sub ----
+
 #' @rdname ShapeFrame
 #' @export
-setMethod("[", c("ShapeFrame", "numeric"), \(x, i, ...) {
-    j <- seq_len(length(x))[i]
-    x@data <- x@data[j, ]
+setMethod("[", c("ShapeFrame", "missing", "ANY"), 
+    \(x, i, j, ...) x[seq_len(nrow(x)), j])
+
+#' @rdname ShapeFrame
+#' @export
+setMethod("[", c("ShapeFrame", "ANY", "missing"), 
+    \(x, i, j, ...) x[i, seq_len(ncol(x))])
+
+#' @rdname ShapeFrame
+#' @export
+setMethod("[", c("ShapeFrame", "missing", "missing"), 
+    \(x, i, j, ...) x[seq_len(nrow(x)), seq_len(ncol(x))])
+
+#' @rdname ShapeFrame
+#' @export
+setMethod("[", c("ShapeFrame", "numeric", "numeric"), \(x, i, j, ...) { 
+    i <- seq_len(nrow(x))[i]
+    j <- seq_len(nrow(x))[j]
+    x@data <- x@data[i, j]
     return(x)
 })
