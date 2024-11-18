3d42fed58e2fb66845371e1e4ef8d4acc53887d3
HelenaLC <helena@crowells.eu>
2024-11-18 08:47:11 +0100

    ... argument tests

--- tests/testthat/test_query.R
+++ tests/testthat/test_query.R
@@ -4,8 +4,15 @@ x <- readSpatialData(x, tables=FALSE)
 
 test_that("bounding box", {
     d <- dim(i <- image(x))
+    # missing bounding box coordinates
+    expect_error(query(i, xmin=0, xmax=1, ymin=0))
+    # invalid coordinate space
+    expect_error(query(i, ".", xmin=0, xmax=1, ymin=0, ymax=1))
+    expect_error(query(i, 100, xmin=0, xmax=1, ymin=0, ymax=1))
     # neither crop nor shift
     expect_identical(query(i, xmin=0, xmax=d[3], ymin=0, ymax=d[2]), i)
+    # order is irrelevant
+    expect_identical(query(i, ymax=d[2], xmax=d[3], xmin=0, ymin=0), i)
     # crop but don't shift
     j <- query(i, xmin=0, xmax=w <- d[3]/2, ymin=0, ymax=h <- d[2]/4)
     expect_equal(dim(j), c(3, h, w)) 
