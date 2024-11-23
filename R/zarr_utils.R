d3d17d35e9ba04fc8b0c8aca0f39223c5864d917
Artur-man <artur-man@hotmail.com>
2024-11-23 15:27:30 +0100

    export getZarrArrayPath

--- R/zarr_utils.R
+++ R/zarr_utils.R
@@ -1,6 +1,13 @@
 # TODO: temporary path getter for Rarr::ZarrArray
+
+#' getZarrArrayPath
+#' 
+#' returns the path of a zarr store associated with ZarrArray
+#' 
+#' @param a ZarrArray
+#' 
 #' @importFrom methods slotNames
-#' @noRd
+#' @export
 getZarrArrayPath <- \(a){
   if("zarr_array_path" %in% methods::slotNames(a))
     return(a@zarr_array_path)
