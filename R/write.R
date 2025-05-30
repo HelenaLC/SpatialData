4eb4fe1dc4a9d51b98b9787fc220635b7b0fa0d0
Artur-man <artur-man@hotmail.com>
2025-05-30 23:53:15 +0200

    move zarr functions

--- R/write.R
+++ R/write.R
@@ -53,36 +53,3 @@ writePoint <- function(x, name, path, replace = TRUE, version = "v2") {
   # write data
   arrow::write_dataset(data(x), file.path(zarr.group, "points.parquet"))
 }
-
-.replace_zarr <- function(name, path, replace, version = "v2")
-{
-  zarr.path <- file.path(path,name)
-  if (dir.exists(zarr.path) && !replace)
-    stop("zarr store with name ", zarr.path ," doesnt exist")
-  if (!replace)
-    stop("Directory \"", zarr.path, "\" already exists. ",
-         "Use 'replace=TRUE' to replace it. ",
-         "Its content will be lost!")
-  if (unlink(zarr.path, recursive=TRUE) != 0L)
-    stop("failed to delete directory \"", dir, "\"")
-  create_zarr(name, path, version = version)
-  return(zarr.path)
-}
-
-.make_zarr_group <- function(x, name, path, replace, version){
-  gd <- file.path(path, "points")
-  if(!dir.exists(gd))
-    dir.create(gd)
-  ng <- file.path(gd, name)
-  if(replace){
-    unlink(ng, recursive = TRUE)
-  } else {
-    nms <- list.dirs(file.path(gd), full.names = FALSE)
-    if(name %in% nms)
-      stop("Directory \"", ng, "\" already exists. ",
-           "Use 'replace=TRUE' to replace it. ",
-           "Its content will be lost!")
-  }
-  create_zarr_group(gd, name, version)
-  return(ng)
-}
