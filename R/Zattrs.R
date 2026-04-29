27ea99be9882d65bf5de0e84583155d007f06dcb
HelenaLC <helena@crowell.eu>
2026-04-29 19:39:32 +0200

    move zattrs show to misc

--- R/Zattrs.R
+++ R/Zattrs.R
@@ -127,49 +127,6 @@ setMethod("multiscales", "list", .ms)
 #' @export
 setMethod("channels", "Zattrs", \(x, ...) .ch(x))
 
-#' @importFrom S4Vectors coolcat
-.showZattrs <- function(object) {
-    cat("class: Zattrs\n")
-    ax <- axes(object)
-    cat(sprintf("axes(%d):\n", length(ax)))
-    if (is.character(ax[[1]])) {
-        cat("- name:", unlist(ax), "\n")
-    } else {
-        cat("- name:", vapply(ax, \(.) .$name, character(1)), "\n")
-        cat("- type:", vapply(ax, \(.) .$type, character(1)), "\n")
-    }
-    # TODO: more detailed 'sequence' display
-    cat(sprintf("coordTrans(%d):\n", n <- length(CTname(object))))
-    g <- \(.) {
-        . <- paste(unlist(.), collapse=",")
-        if (!grepl(",", ., fixed = TRUE)) return(.)
-        sprintf("[%s]", .)
-    }
-    f <- \(.) {
-        if (is.null(.)) return("")
-        paste0(":", g(lapply(., g)))
-    }
-    for (i in seq_len(n))
-        cat(sprintf("- %s: (%s%s)\n",
-            CTname(object)[i],
-            CTtype(object)[i],
-            f(CTlist(object)[[i]][[CTtype(object)[i]]])))
-    ms <- object$multiscales[[1]]
-    if (!is.null(ms)) {
-        ds <- ms$datasets
-        ps <- vapply(ds, \(.) .$path, character(1))
-        coolcat("datasets(%d): %s\n", ps)
-        for (i in seq_along(ds)) {
-            ct <- ds[[i]]$coordinateTransformations[[1]]
-            cat(sprintf("- %s: (%s:%s)\n", 
-                ps[i], ct$type, g(ct[[ct$type]]))) 
-        }
-    }
-    cs <- unlist(channels(object))
-    if (!is.null(cs)) coolcat("channels(%d): %s\n", cs)
-}
-setMethod("show", "Zattrs", .showZattrs)
-
 #' @name SDattrs
 #' @title \code{SpatialData} attributes
 #' 
